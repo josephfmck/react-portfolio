6620fe68a933ed4ca570075b29692c0a9e13b323
Joe <josephfmck@gmail.com>
2022-11-29 15:50:07 -0600

    <Logo/> rendered in <Home/>, uses img svg and container, scss .logo-container setup

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 
 import LogoTitle from '../../assets/images/logo-s.png';
 import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo';
 import './index.scss'
 
 const Home = () => {
@@ -47,7 +48,8 @@ const Home = () => {
                 <h2>Frontend Developer / Javascript Developer</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
-        </div>
+            <Logo />
+        </div> 
      );
 }
 
