c3deb8cfb4e62760df0bc248c877abf8f7ba1f9c
Joe <josephfmck@gmail.com>
2023-01-02 19:06:36 -0600

    API KEY setup, installed dependencies, create backend.js for backend server to continously run

---
+++ backend.js
@@ -0,0 +1,33 @@
+//CUSTOM - backend for keeping sensitive data safe
+//npm i express cors dotenv axios 
+//npm i rate limiter
+//TERMINAL: nodemon backend.js - continuously run backend server
+
+const PORT = 8000;
+
+//import express and cors
+const express = require('express');
+const cors = require('cors');
+//import dotenv and axios
+const axios = require('axios');
+require('dotenv').config();
+//rate limiter to help with security?
+// const rateLimit = require("express-rate-limit");
+
+//listen to port 8000, call express(), callback function
+const app = express();
+
+app.use(cors())
+
+app.get('/', (req,res) => {
+    res.json('hi')
+})
+
+
+
+
+
+
+app.listen(8000, ()=>{
+    console.log(`Listening on port ${PORT}`);
+})
