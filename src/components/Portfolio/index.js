38f66d985c06128f6f6f1cda405ae0dbce79a715
Joe <josephfmck@gmail.com>
2023-01-09 16:46:17 -0600

    project content scss, transition setup, btn hover setup

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -25,6 +25,15 @@ const Portfolio = () => {
                 className="portfolio-image"
                 src={port.cover} 
                 alt={port.title} />
+                <div className='content'>
+                  <p className='title'>{port.title}</p>
+                  <h4 className='description'>{port.description}</h4>
+                  <button 
+                  className='btn'
+                  onClick={() => {window.open(port.url)}}>
+                  View
+                  </button>
+                </div>
               </div>
             )
           })
