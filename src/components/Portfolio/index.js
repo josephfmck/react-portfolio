2ca7f0a43d9bbcc4e37b66ee6d6e5a90d206c532
Joe <josephfmck@gmail.com>
2024-04-18 20:56:44 -0500

    test changes for images

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -18,11 +18,25 @@ const Portfolio = () => {
     
     return (
       <div className='images-container'>
+        <div className='image-box' key={idx}>
+                <img 
+                className="portfolio-image"
+                src="https://josephfmck.github.io/react-portfolio/portfolio/ButterPort.png"
+                alt={port.title} />
+                <div className='content'>
+                  <p className='title'>{port.title}</p>
+                  <h4 className='description'>{port.description}</h4>
+                  <button 
+                  className='btn'
+                  onClick={() => {window.open(port.url)}}>
+                  View
+                  </button>
+                </div>
+              </div>
         {
           portfolioDataArr.map((port, idx) => {
-            console.log(port);
-            console.log(port.cover);
-            console.log(process.env.PUBLIC_URL + '/' + port.cover);
+            console.log("Image URL:", `${process.env.PUBLIC_URL}/${port.cover}`);
+
             return (
               <div className='image-box' key={idx}>
                 <img 
