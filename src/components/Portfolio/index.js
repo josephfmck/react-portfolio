6b135995e7ff23b0f9c5cb3c28b30a1717d2fcec
Joe <josephfmck@gmail.com>
2024-04-18 21:10:40 -0500

    image deploy update

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -13,35 +13,29 @@ const Portfolio = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
   console.log(portfolioData);
 
+    // Function to determine the image source URL
+    const getImageUrl = (cover) => {
+      if (process.env.NODE_ENV === 'production') {
+        return `https://josephfmck.github.io/react-portfolio/${cover}`;
+      } else {
+        return `${process.env.PUBLIC_URL}/${cover}`;
+      }
+    };
+  
   //maps through data and renders portfolio items
   const renderPortfolio = (portfolioDataArr) => {
-    
+    console.log("Image URL:", getImageUrl(port.cover)); // Logging image URL for debugging
+
     return (
       <div className='images-container'>
-        <div className='image-box' key={idx}>
-                <img 
-                className="portfolio-image"
-                src="https://josephfmck.github.io/react-portfolio/portfolio/ButterPort.png"
-                alt={port.title} />
-                <div className='content'>
-                  <p className='title'>{port.title}</p>
-                  <h4 className='description'>{port.description}</h4>
-                  <button 
-                  className='btn'
-                  onClick={() => {window.open(port.url)}}>
-                  View
-                  </button>
-                </div>
-              </div>
         {
           portfolioDataArr.map((port, idx) => {
             console.log("Image URL:", `${process.env.PUBLIC_URL}/${port.cover}`);
-
             return (
               <div className='image-box' key={idx}>
                 <img 
                 className="portfolio-image"
-                src={process.env.PUBLIC_URL + '/' + port.cover}
+                src={getImageUrl(port.cover)}
                 alt={port.title} />
                 <div className='content'>
                   <p className='title'>{port.title}</p>
